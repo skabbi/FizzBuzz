e2250b7c3b16f36862962490d3c0eba3178bf225
Skarphedinn Thordarson <skarphedinnth@gmail.com>
2019-04-17 02:16:28 +0000

    Remocing loop and making more functional

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,21 @@
-function fizzBuzz(start, end) {
-  for (let i = start; i <= end; i++) {
-    let string = "";
-
-    if (i % 3 === 0) {
-      string += "Fizz";
-    }
-    if (i % 5 === 0) {
-      string += "Buzz";
-    }
+const fizzBuzz = (start, end) => {
+  const numbers = intRange(start, end);
+  numbers.forEach(i => {
+    let string = '';
+    if (i % 3 === 0) { string += 'Fizz' };
+    if (i % 5 === 0) { string += 'Buzz' };
 
     console.log(string || i);
-  }
+  })
+}
+
+// [start, start + 1, ..., end]
+const intRange = (start, end) => {
+  const size = end - start + 1;
+  return Array.from(new Array(size), (x,i) => i + start);
 }
 
 // Checks for start and end command line argument flags
-let start = (process.argv.indexOf("-start") != -1) ?  parseInt(process.argv[process.argv.indexOf("-start") + 1]) : 0;
-let end = (process.argv.indexOf("-end") != -1) ?  parseInt(process.argv[process.argv.indexOf("-end") + 1]) : 100;
+const start = (process.argv.indexOf('-start') != -1) ? parseInt(process.argv[process.argv.indexOf('-start') + 1]) : 0;
+const end = (process.argv.indexOf('-end') != -1) ? parseInt(process.argv[process.argv.indexOf('-end') + 1]) : 100;
 fizzBuzz(start, end);
